ef96cc25fd8376144bf675c58474523464a8f2f2
akif-mehmood <muhammadakif148@gmail.com>
2025-08-08 12:31:39 +0500

    br tag update

--- apps.js
+++ apps.js
@@ -8,7 +8,7 @@ var name, age, country;
     document.writeln(`${legal} <br>`); 
     document.writeln(illegal);
 
-    document.writeln("<p><b>Variable names can only contain</b> letters, numbers, $ and _. <br>For example: <code>$my_1stVariable</code></p>");
-    document.writeln("<p>Variables must begin with a <b>letter, $ or _. <br>For example: <code>$name, _name or name</b></p>");
+    document.writeln("<p>Variable names can only contain letters, <b>numbers, $ and _. <br>For example: <code>$my_1stVariable</code></b></p>");
+    document.writeln("<p>Variables must begin with a letter, $ or _. <br>For example: <code>$name, _name or name</code></p>");
     document.writeln("<p>Variable names are <b>case sensitive</b></p>");
     document.writeln("<p>Variable names should not be <b>JS keywords</b></p>");
